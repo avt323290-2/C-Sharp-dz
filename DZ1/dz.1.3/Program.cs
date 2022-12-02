a85748b58837dd2706d592f9b0a91f1208ba1855
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-02 20:40:05 +0300

    Добавили решение задачи 6

--- DZ1/dz.1.3/Program.cs
+++ DZ1/dz.1.3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("333Hello, World!");
+﻿/* 
+Задача 6: Напишите программу, которая на вход
+принимает число и выдаёт, является ли число чётным 
+(делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.Clear();
+Console.Write("Введите число ");
+string number = Console.ReadLine() ?? "";
+int N = Convert.ToInt32(number);
+if (N % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else 
+{
+    Console.WriteLine("Число является нечетным");
+}
